690ebedb3552ffb5cbf52be5f2948acca3e46eb4
thallegacy <thallegacy@users.noreply.github.com>
2021-03-06 08:27:26 -0500

    svg canvas dimensions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-// @TODO: YOUR CODE HERE!
+// Set the width, height amd margins for the SVG canvas
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
