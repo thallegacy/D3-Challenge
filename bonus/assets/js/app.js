9587030dfbed2613b93f40cded152eb0a081832f
thallegacy <thallegacy@users.noreply.github.com>
2021-03-07 09:49:04 -0500

    render circles

--- bonus/assets/js/app.js
+++ bonus/assets/js/app.js
@@ -74,3 +74,13 @@ function renderYAxes(newYScale, yAxis) {
 
   return yAxis;
 }
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
+  circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]))
+      .attr("cy", d => newYScale(d[chosenYAxis]));
+  
+  return circlesGroup;
+}
